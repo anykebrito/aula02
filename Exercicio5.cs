29d678f22e75ff7d773e8da5502e8053cbe08907
anykebrito <anyke.brito@edu.senai.br>
2026-02-19 15:04:25 -0300

    entregando exercicios 4, 5, 6 e 7

---
+++ Exercicio5.cs
@@ -0,0 +1,13 @@
+// -- Arquivo Exercicio5.cs
+using System.Runtime.CompilerServices;
+
+public static class Exercicio5
+{
+    public static void Executar()
+    {
+        string nome = "Anyke";
+        string estilomusical = "pop";
+        string cantor = "Doja Cat";
+        Console.WriteLine ($"Eu sou {nome}, curto o estilo musical {estilomusical} e meu artista favorito desse estilo é {cantor}");
+    }
+}
---
+++ Exercício4.cs
@@ -0,0 +1,15 @@
+// -- Arquivo Exercicio4.cs
+using System.Runtime.CompilerServices;
+
+public static class Exercicio4
+{
+    public static void Executar()
+    {
+        string nome = "Anyke";
+        int idade = 16;
+        string hobby1 = "praticar esportes";
+        string hobby2 = "ler";
+        string graduacao = "medicina";
+        Console.WriteLine ($"Meu nome é {nome}, tenho {idade} anos, meus dois passatempos favoritos são {hobby1} e {hobby2}, e pretendo cursar a graduação em {graduacao}");
+    }
+}
