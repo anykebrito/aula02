678d7875bad3c9d9998245ce901ba62599977aa3
anykebrito <anyke.brito@edu.senai.br>
2026-02-19 11:32:06 -0300

    Entrega, exs: 1, 2, 3

---
+++ Exercicio2.cs
@@ -0,0 +1,12 @@
+// -- Arquivo Exercicio2.cs
+public class Exercicio2
+{
+    //private static string nome = "Anyke Brito";
+    
+    public static void Executar()
+    {
+        string nome = "Anyke Brito";
+        Console.WriteLine(nome);
+        Console.WriteLine();
+    }
+}
